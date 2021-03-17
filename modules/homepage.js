83b73d85f82cbe5418a59e90f963b7b1659a5be9
lasergangers <celloadrian29@gmail.com>
2021-03-17 23:53:14 +0700

    render searchbar and iconbutton module, style header and modules, searchbar is currently non functional

--- modules/homepage.js
+++ modules/homepage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {View, StyleSheet, Text, StatusBar} from 'react-native'
+import SearchBar from './searchbar'
 import theme from './theme'
+import IconButton from './iconbutton'
 
 const Homepage = () => {
     return (
@@ -8,18 +10,42 @@ const Homepage = () => {
         <StatusBar barStyle='dark-content' backgroundColor = {theme.light}/>
         <View style = {styles.container}>
             <Text style = {styles.header}>Note Screen</Text>
+            <SearchBar containerStyle={{marginVertical: 15}}/>
+            <View style = {[StyleSheet.absoluteFillObject ,styles.emptyHeaderContainer]}> 
+                <Text style = {styles.emptyHeader}>Add Notes</Text>
+                <IconButton antIconName = 'plus' style = {styles.addButton}/>
+            </View>
         </View>
         </>
     )
 }
 
+// TODO: dynamic font sizes, by pixel ratio or phone dimensions
 const styles = StyleSheet.create({
     container: {
         paddingHorizontal: 20,
+        flex: 1,
     },
     header: {
-        fontSize: 40, // TODO: dynamic font sizes, by pixel ratio or phone dimensions
+        fontSize: 40,
         fontWeight: 'bold',
+    },
+    emptyHeaderContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: -1,
+    },
+    emptyHeader: {
+        fontSize: 30,
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        opacity: 0.2,
+    },
+    addButton: {
+        position: 'absolute',
+        right: 15,
+        bottom: 50,
     }
 })
 
