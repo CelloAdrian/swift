b233582343608a27eafc183f914bfbe2634826a8
lasergangers <celloadrian29@gmail.com>
2021-03-17 23:52:33 +0700

    style iconbutton module

--- modules/iconbutton.js
+++ modules/iconbutton.js
@@ -3,18 +3,25 @@ import {View, StyleSheet} from 'react-native'
 import {AntDesign} from '@expo/vector-icons'
 import theme from './theme'
 
-const IconButton = ({antIconName, size, color}) => {
+const IconButton = ({antIconName, size, color, style, onPress}) => {
     return (
         <AntDesign
         name = {antIconName}
         size = {size || 24}
-        theme = {color || theme.light}
+        color = {color || theme.light}
+        style = {[styles.icon, {...style}]}
+        onPress = {onPress}
         />
     )
 }
 
 const styles = StyleSheet.create({
-    container:{}
+    icon: {
+        backgroundColor: theme.primary,
+        padding: 15,
+        borderRadius: 50,
+        elevation: 5, // might need to use other shadow properties as this might not work properly on iOS
+    }
 })
 
 export default IconButton
